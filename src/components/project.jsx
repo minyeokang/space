cfd5cb5f65fcd009168b5a18903a3ba3e32b0f40
minyeo kang <dobieborie1@gmail.com>
2022-05-11 09:54:24 +0900

    edit

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -42,7 +42,7 @@ function Project() {
                     </div>
                     <div className={styles.txt}>
                         <h2>Question</h2>
-                        <p className={styles.bt}>“보리는 전라남도 영광 백수해안도로에 위치한다.<hr />
+                        <p className={styles.bt}>“보리는 전라남도 영광 백수해안도로에 위치한다.<br />
                             이곳의 지리적 특성은 어떠한가?”</p>
                         <h2 className={styles.an}>Answer</h2>
                         <p className={styles.an}>“ 해안도로를 따라 오른쪽에 바다를 끼고 현장으로<br /> 다가갔는데, 도로에서 3~4m 낮은 곳에 대지가 위<br /> 치해 있었다. 주위에 높은 건물이 없어 시원하게 바<br /> 다를 볼 수 있고, 절벽가여서 바다로 가까이 다가가<br /> 면 파도 소리까지 기분 좋게 들을 수 있었다. 대지<br /> 는 야트막한 경사지로 보리농사를 짓는 밭으로 사<br /> 용되고 있었다. ”</p>
@@ -52,7 +52,7 @@ function Project() {
                 <div className={styles.qna} >
                     <div className={styles.txt}>
                         <h2>Question</h2>
-                        <p className={styles.bt}>“구불구불한 여정을 거쳐 도착한 내부 공간은 방향성이<br />  굉장히 명확하다. 건물의 배치, 바닥 레벨의 차이,<br />
+                        <p className={styles.bt}>“구불구불한 여정을 거쳐 도착한 내부 공간은 방향성이<br /> 굉장히 명확하다. 건물의 배치, 바닥 레벨의 차이,<br />
                             직접 디자인한 가구의 형태와 배치 등 모든 것들이 바다<br /> 를 향하고 있다. 이곳에서 사람들이 어떤 경험을 하길 바<br /> 라며 계획했는가? ”</p>
                         <h2 className={styles.an}>Answer</h2>
                         <p className={styles.an}>“도심 속에 있는 카페들처럼 유명한 디저트를 먹기<br />위해서 오고, 친구를 만나 왁자지껄 떠들기 위해서<br /> 오고, 이런 것과는 거리가 멀다고 봤다.<br />
